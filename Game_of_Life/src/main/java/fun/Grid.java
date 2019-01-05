ed9d6fd6facbbd5a0c8110897d98a2e56b17141b
Brian Sjoberg <brian_sjoberg@yahoo.com>
2019-01-05 11:42:35 -0500

    Initial grid work.

---
+++ Game_of_Life/src/main/java/fun/Grid.java
@@ -0,0 +1,12 @@
+package fun;
+
+public class Grid {
+	int xSize = 0;
+	int ySize = 0;
+	
+	public Grid (int xSize, int ySize) {
+		this.xSize = xSize;
+		this.ySize = ySize;
+	}
+	
+}
--- Game_of_Life/src/test/java/fun/GridSteps.java
+++ Game_of_Life/src/test/java/fun/GridSteps.java
@@ -7,9 +7,13 @@ import cucumber.api.java.en.When;
 
 public class GridSteps {
 	@Given("^(\\d+) by (\\d+) grid with (\\d+) alive cell$")
-	public void byGridWithAliveCell(int arg1, int arg2, int arg3) throws Throwable {
+	public void byGridWithAliveCell(int xSize, int ySize, int numberOfAliveCells) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    Grid grid = new Grid(xSize, ySize);
+	    
+	    if (numberOfAliveCells == 1) {
+	    	// Set top left of cell alive
+	    }
 	}
 
 	@When("^game ticks$")
