7c500d9b49e34aa390d1fae69643230a51fc1a12
Brian Sjoberg <brian_sjoberg@yahoo.com>
2019-01-12 11:38:56 -0500

    Added board variable and initialize board from datatable.

--- Game_of_Life/src/test/java/fun/GridSteps.java
+++ Game_of_Life/src/test/java/fun/GridSteps.java
@@ -1,5 +1,9 @@
 package fun;
 
+import java.util.List;
+
+import org.junit.Assert;
+
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
@@ -7,28 +11,35 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class GridSteps {
+	private List<List<String>> board;
+	
 	@Given("^a board like this:$")
-	public void aBoardLikeThis(DataTable arg1) throws Throwable {
+	public void aBoardLikeThis(DataTable table) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    // For automatic transformation, change DataTable to one of
 	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
 	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
+
+		this.board = table.raw();
 	}
 
 	@When("^game ticks$")
 	public void gameTicks() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
+		System.out.println(board.toString());
+		
 	    throw new PendingException();
 	}
 	
 	@Then("^the board should look like this:$")
-	public void theBoardShouldLookLikeThis(DataTable arg1) throws Throwable {
+	public void theBoardShouldLookLikeThis(DataTable table) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    // For automatic transformation, change DataTable to one of
 	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
 	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
+
+		//Assert.assertTrue(table.raw().equals(board));
+		throw new PendingException();
 	}
 
 }
